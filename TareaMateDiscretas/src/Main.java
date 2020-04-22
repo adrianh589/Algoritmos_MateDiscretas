918bd0cfd8f6046ca5ce362a3a850269ad9d1a98
Adrian Hoyos <33093678+adrianh589@users.noreply.github.com>
2020-04-22 12:19:39 -0500

    Ordenado de RSA para lectura de datos facil

--- TareaMateDiscretas/src/Main.java
+++ TareaMateDiscretas/src/Main.java
@@ -9,7 +9,6 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        RSA rsa = new RSA();
 
         //Para imprimir numeros primos
         //Es_primo.imprimirPrimo();
@@ -24,48 +23,11 @@ public class Main {
         //System.out.println(Criptografia_Ejemplo_1.desencriptar("FDWUIEAGEIVDI"));
 
         //RSA
-        //String[][] alfabeto = RSA.alfabetoEspanol;//Descomenta esta linea para usar el español
-        String[][] alfabeto = RSA.alfabetoIngles;//Descomenta esta linea para usar el gringo :D
+        String mensaje = "1420 0614 1301 1694";
+        RSA rsa = new RSA("desencriptar", mensaje, "ingles", 47, 61, 2867, 11, -1, -1);
 
-        int p = 59;//Selecciona tu p
 
-        int q = 101;//Selecciona tu q
-
-        //int z = rsa.calcularZ(p,q);
-        int z = 2867;//Descomenta esta linea para ponerla manualmente
-        System.out.println("z: "+z);
-
-        int n = 11;//Selecciona tu n
-        //int n = rsa.calcularN( 2,FI );//en caso de quererlo calcular automaticamente, descomenta esta linea
-
-        int FI = rsa.calcularFI(p,q);
-        System.out.println("FI: "+FI);
-
-        System.out.println("n: "+n);
-
-        int s = rsa.calcularS(n, FI);
-        System.out.println("S: "+s);
-
-//        System.out.println("Mensaje convertido a string RSA (No encriptado)");
-//
-//        String volverStringRSA = rsa.volverStringRSA("EL FINAL ESTA PROXIMO",
-//                alfabeto);//Esto es para poner el numero que corresponde a cada letra
-//
-//        System.out.println(volverStringRSA+"\n");
-//
-//        System.out.println("Mensaje encriptado con RSA");
-//        String encriptarRSA = rsa.encriptarRSA(volverStringRSA, n, z);
-//        System.out.println(encriptarRSA+"\n");//Aqui se encripta el mensaje tomando el volverStringRSA()
-//
-        System.out.println("Mensaje desencriptado con RSA");
-        String desencriptarRSA = rsa.desencriptarRSA(s, z, "1420 0614 1301");
-        System.out.println(desencriptarRSA+"\n");//Aqui se desencripta el RSA, volviendolo a las letras que corresponde
-//
-        System.out.println("Mensaje desencriptado a String");
-        System.out.println(rsa.descifrarStringRSAaLetras(desencriptarRSA, alfabeto));
-//        //Aqui se vuelve el encriptamiento en Strings (No en RSA) y se devuelven las letras originales
     }
-
 
 
 }
--- TareaMateDiscretas/src/operaciones/RSA.java
+++ TareaMateDiscretas/src/operaciones/RSA.java
@@ -7,15 +7,152 @@ public class RSA {
     static Euclides euclides = new Euclides();
     static Es_primo primo = new Es_primo();
 
-    public static String[][] alfabetoEspanol = {
+    private static String[][] alfabetoEspanol = {
             {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"},
             {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"}
     };
 
-    public static String[][] alfabetoIngles = {
+    private static String[][] alfabetoIngles = {
             {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"},
-            {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"}
+            {"00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25"}
     };
+
+    /**
+     * Funcion para calcular en RSA
+     * @param accion "encriptar": Encripta el mensaje en RSA "Desencriptar": Desencripta el mensaje "ambas":Enripta y desencripta el mensaje
+     * @param frase Frase a hacerle la acción
+     * @param idioma Eleccion del idioma del lenguaje "español" o "ingles"
+     * @param p Eleccion de la p, debe ir si o si
+     * @param q Eleccion de la p, debe ir si o si
+     * @param z -1 para elegirlo automaticamente, en otro caso colocar el valor
+     * @param n -1 para elegirlo automaticamente, en otro caso colocar el valor
+     * @param FI -1 para elegirlo automaticamente, en otro caso colocar el valor
+     * @param s -1 para elegirlo automaticamente, en otro caso colocar el valor
+     */
+    public RSA(String accion, String frase, String idioma, int p, int q, int z, int n, int FI, int s){
+
+        String[][] alfabeto = eleccionDeAlfabeto(idioma);
+        accion = accion.toLowerCase();
+        frase = anadirX(frase.toUpperCase());
+
+        System.out.println("La frase es: \n"+frase);
+
+        if(idioma.equals("español")){
+            System.out.println("Idioma elegido: Español");
+        }else if(idioma.equals("ingles")){
+            System.out.println("Idioma elegido: Ingles");
+        }else {
+            System.err.println("ERROR CON EL IDIOMA");
+        }
+
+        System.out.println("p: "+p);
+        System.out.println("q: "+q);
+
+        if(z == -1){
+            z = calcularZ(p,q);
+        }
+        System.out.println("z: "+z);
+
+        if(n == -1){
+            n = calcularN( 2,FI );
+        }
+        System.out.println("n: "+n);
+
+        if(FI == -1){
+            FI = calcularFI(p, q);
+        }
+        System.out.println("FI: "+FI);
+
+        if(s == -1){
+            s = calcularS(n, FI);
+        }
+        System.out.println("S: "+s);
+
+        switch (accion){
+            case "encriptar":
+
+                System.out.println("\nRepresentacion en RSA:");
+
+                //Esto es para poner el numero que corresponde a cada letra
+                String representacionEnRSA = representarEnRSA(frase,
+                        alfabeto);
+
+                System.out.println(representacionEnRSA+"\n");
+                System.out.println("--------------------------------------");
+
+
+                //Aqui se encripta el mensaje tomando el representarEnRSA()
+                System.out.println("encriptacion con RSA");
+                String encriptacionRSA = encriptarRSA(representacionEnRSA, n, z);
+                System.out.println(encriptacionRSA+"\n");
+                System.out.println("--------------------------------------");
+
+                break;
+            case "desencriptar":
+
+                System.out.println("desencriptacion con RSA");
+                String desencriptarRSA = desencriptarRSA(s, z, frase);
+                System.out.println(desencriptarRSA+"\n");//Aqui se desencripta el RSA, volviendolo a las letras que corresponde
+                System.out.println("--------------------------------------");
+
+                //Aqui se vuelve el encriptamiento en Strings (No en RSA) y se devuelven las letras originales
+                System.out.println("Representacion RSA en letras");
+                System.out.println(representarRSAenLetras(desencriptarRSA, alfabeto));
+
+                break;
+            case "ambas":
+                System.out.println("\nRepresentacion en RSA:");
+
+                //Esto es para poner el numero que corresponde a cada letra
+                String representarEnRSA = representarEnRSA(frase,
+                      alfabeto);
+
+                System.out.println(representarEnRSA+"\n");
+                System.out.println("--------------------------------------");
+
+
+                //Aqui se encripta el mensaje tomando el representarEnRSA()
+                System.out.println("encriptacion con RSA");
+                String encriptarRSA = encriptarRSA(representarEnRSA, n, z);
+                System.out.println(encriptarRSA+"\n");
+                System.out.println("--------------------------------------");
+
+                System.out.println("desencriptacion con RSA");
+                String desencriptacionRSA = desencriptarRSA(s, z, encriptarRSA);
+                System.out.println(desencriptacionRSA+"\n");//Aqui se desencripta el RSA, volviendolo a las letras que corresponde
+                System.out.println("--------------------------------------");
+
+                //Aqui se vuelve el encriptamiento en Strings (No en RSA) y se devuelven las letras originales
+                System.out.println("Representacion RSA en letras");
+                System.out.println(representarRSAenLetras(desencriptacionRSA, alfabeto));
+                break;
+            default:
+                System.out.println("Accion no válida");
+
+        }
+
+    }
+
+    /**
+     * Funcion para seleccionar el idioma de RSA
+     * @param idioma español o ingles
+     * @return devuelve el idioma seleccionado
+     */
+    private static String[][] eleccionDeAlfabeto(String idioma){
+        String[][] alfabeto = null;
+        idioma = idioma.toLowerCase();
+        //System.out.println("Selecciona tu idioma: \n1. Español\n2. Ingles");
+        if(idioma.equals("español")){
+            alfabeto = alfabetoEspanol;
+        }else if(idioma.equals("ingles")){
+            alfabeto = alfabetoIngles;
+        }else {
+            System.err.println("Idioma Inválido");
+        }
+        return alfabeto;
+    }
+
+
 
     public static int calcularZ(int p, int q) {
         return p * q;
@@ -59,7 +196,7 @@ public class RSA {
      * @param frase
      * @return
      */
-    public static String volverStringRSA(String frase, String[][] alfabeto) {
+    public static String representarEnRSA(String frase, String[][] alfabeto) {
         String resultado = "";
         frase = frase.replace(" ", "");//Limpiamos los espacios
         int separador = 0;
@@ -76,10 +213,6 @@ public class RSA {
                 resultado += " ";
             }
         }
-
-        if (!(frase.length() % 2 == 0)) {//Si es impar la cadena le agregamos X que equivale a 24
-            resultado += "24";
-        }
         return resultado;
     }
 
@@ -89,7 +222,7 @@ public class RSA {
      * @param alfabeto
      * @return
      */
-    public static String descifrarStringRSAaLetras(String fraseEncriptada, String[][] alfabeto) {
+    public static String representarRSAenLetras(String fraseEncriptada, String[][] alfabeto) {
         String fraseCopiada = fraseEncriptada;
         fraseCopiada = fraseCopiada.replace(" ", "");
         String resultado = "";
@@ -172,6 +305,20 @@ public class RSA {
             }
         }
         return resultado;
+    }
+
+    /**
+     * Anadir la X si la cadena es impar
+     * @param frase
+     * @return
+     */
+    public static String anadirX(String frase){
+        String aux = frase;
+        aux = aux.replace(" ", "");//Limpiamos los espacios
+        if (!(aux.length() % 2 == 0)) {//Si es impar la cadena, le agregamos X
+            frase += "X";
+        }
+        return frase;
     }
 
 }
